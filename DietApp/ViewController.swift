5a0451a780363e3fe89693f6ab5b0a1a891db830
Mika <iluvkatyperry1126@gmail.com>
2018-10-02 09:43:09 +0900

    Initial Commit

---
+++ DietApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DietApp
+//
+//  Created by Mika on 2018-10-02.
+//  Copyright © 2018 Mika. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
